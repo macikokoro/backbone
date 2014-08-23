7f81c6189437d298c0758b4283a4969960459458
Joaquin <betwinsouls@gmail.com>
2014-08-22 18:11:53 -0700

    view now prints li

--- app/view.js
+++ app/view.js
@@ -3,7 +3,7 @@ var $ = require('jquery');
 Backbone.$ = $;
 
 
-var SimpleView = Backbone.View.extend({});
+var SimpleView = Backbone.View.extend({tagName: 'li'});
 var simpleView = new SimpleView();
 
 console.log(simpleView.el);
